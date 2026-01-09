14320d5fe8905f4146d84ff88495894b1de74981
Santo1215 <jesus2221922@correo.uis.edu.co>
2026-01-09 11:44:19 -0500

    Cambio server.js

--- server.js
+++ server.js
@@ -27,36 +27,6 @@ app.listen(PORT, () => {
   console.log(`🔥 Servidor corriendo en puerto ${PORT}`);
 });
 
-app.post('/login', async (req, res) => {
-  const { usuario, contrasena } = req.body;
-
-  if (!usuario || !contrasena) {
-    return res.status(400).json({ message: 'Faltan datos' });
-  }
-
-  try {
-    const result = await pool.query(
-      'SELECT * FROM dirigentes WHERE usuario = $1 AND contrasena = $2',
-      [usuario, contrasena]
-    );
-
-    if (result.rows.length === 0) {
-      return res.status(401).json({ message: 'Usuario o contrasena incorrectos' });
-    }
-
-    const user = result.rows[0];
-
-    res.json({
-      message: 'Login correcto',
-      user: {
-        id: user.id,
-        nombre: user.nombre,
-        usuario: user.usuario,
-      },
-    });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error del servidor' });
-  }
+app.post('/login', (req, res) => {
+  res.json({ message: 'LOGIN OK SIN DB' });
 });
