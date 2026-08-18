6e8949e699aa1ff7ac0f5dc3692e2a2a4120c2f4
Santo1215 <jesus2221922@correo.uis.edu.co>
2026-08-17 22:32:54 -0500

    Arreglo server.js Mailto

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import pool from './db.js';
 import auth from './auth.js';
 
 webpush.setVapidDetails(
-  'exodojpll29@gmail.com',
+  'mailto:[exodojpll29@gmail.com]',
   process.env.VAPID_PUBLIC_KEY,
   process.env.VAPID_PRIVATE_KEY
 );
